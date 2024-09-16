50a86512bdc94a78377901da80ef65bc914c61f5
The-town <heuchannsight@yahoo.co.jp>
2024-09-16 13:11:05 +0900

    [更新]　リファクタリング

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 import component
-
-
-env = Environment(
-    loader=FileSystemLoader("test_app"),
-    autoescape=select_autoescape()
-)
-
-template = env.get_template("mytemplate.txt")
 
 window = component.Window()
 window.root.mainloop()
